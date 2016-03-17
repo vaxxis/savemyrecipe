2b3ef0219abf324caf2aaf9bb8d9018f8be34352
Vasile Botoroga <vbotoroga@consulenti.schema31.it>
2016-03-17 15:35:37 +0100

    updated email template

--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -1,1 +1,7 @@
-Click here to reset your password: <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
+@extends('auth.emails.layout')
+
+@section('content')
+
+    Click here to reset your password: <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
+
+@endsection
